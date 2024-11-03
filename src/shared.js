0cf0c1332eeedcff06c116eb68b793ae8ae30692
yuri <zapor1337@gmail.com>
2024-11-03 18:09:59 +0300

    Major update. Added import urls support and background updates

---
+++ src/shared.js
@@ -0,0 +1,143 @@
+export async function updateTheme(userSettings, theme, checked, customCSS = undefined) {
+    if (!checked && !theme.edited && customCSS === undefined) {
+        delete userSettings[theme.id];
+
+        chrome.storage.local.set({ userSettings });
+        return;
+    }
+
+    let sourceCSS = '';
+
+    const updateValue = {
+        id: theme.id,
+        link: theme.link,
+        checked: checked,
+        compiledCss: await compileCss(
+            customCSS || userSettings[theme.id]?.edited || userSettings[theme.id]?.local
+                ? customCSS || userSettings[theme.id].sourceCSS
+                : (sourceCSS = await loadStyles(theme.cssLink))
+        ),
+        edited: theme.edited || !!customCSS,
+        sourceCSS: customCSS || '',
+        local: theme.local || false,
+        name: theme.name,
+    };
+
+    if (theme.version) updateValue.version = theme.version;
+
+    userSettings[theme.id] = updateValue;
+    chrome.storage.local.set({ userSettings });
+
+    return sourceCSS;
+}
+
+export async function updateThemes(userSettings, repos) {
+    for (const tab of ["Author's", 'Community'])
+        for (const theme of repos[tab]) {
+            const themeSettings = userSettings[theme.id];
+            if (
+                themeSettings &&
+                themeSettings.checked &&
+                !themeSettings.local &&
+                !themeSettings.edited
+            )
+                await updateTheme(userSettings, theme, true);
+        }
+
+    setExtensionSetting('lastThemesUpdate', new Date().getTime());
+}
+
+export async function loadStyles(link) {
+    const href = window.location.href;
+
+    try {
+        const response = await fetch(link);
+        const result = await response.text();
+
+        if (href !== window.location.href) throw '[loadStyles]: throw by href';
+
+        return result;
+    } catch (e) {
+        console.log('[loadStyles], catch:', e);
+    }
+}
+
+export async function compileCss(code = '', recursive = false) {
+    let compiledCss = code;
+
+    const regex = /@import\s+url\("([^"]+)"\)(?:;)?/g;
+
+    const imports = compiledCss.match(regex);
+
+    if (imports) {
+        const promises = imports.map(async (x) =>
+            compileCss(await loadStyles(x.replace(regex, '$1')), true)
+        );
+
+        const results = await Promise.all(promises);
+
+        console.log('loading @import urls:', promises);
+
+        compiledCss = [results.join('\n'), compiledCss.replace(regex, '')].join('\n');
+    }
+
+    if (recursive) return compiledCss;
+
+    // console.log('compiling code:', compiledCss);
+
+    compiledCss = compiledCss
+        // comment replacement
+        .replace(/\/\*[\s\S]*?\*\//g, '')
+        // minification and !important addition
+        .replace(/;(?!\s*!important)|!important\b|\s+/g, (match) => {
+            if (match === ';') return ' !important;';
+            if (match === '!important !important') return '!important';
+            return ' ';
+        })
+        // removing !important from @font-face imports
+        .replace(/(@font-face\s*{[^}]*})\s*(!\s*important)?/g, (match, p1, p2) => {
+            return match.replaceAll(' !important;', ';');
+        })
+        .trim();
+
+    // console.log('compiled code:', compiledCss);
+
+    return compiledCss;
+}
+
+export async function setUserSetting(key, value) {
+    const userSettings = await getUserSettings();
+
+    userSettings[key] = value;
+
+    chrome.storage.local.set({ userSettings });
+}
+
+export async function getUserSettings() {
+    const userSettings = await new Promise((resolve) =>
+        chrome.storage.local.get('userSettings', (result) => resolve(result.userSettings || {}))
+    );
+    console.log('userSettings', userSettings);
+
+    return userSettings;
+}
+
+export async function setExtensionSetting(key, value) {
+    const extensionSettings = await getExtensionSettings();
+
+    extensionSettings[key] = value;
+
+    chrome.storage.local.set({ extensionSettings });
+}
+
+export async function getExtensionSettings() {
+    const extensionSettings = await new Promise((resolve) =>
+        chrome.storage.local.get('extensionSettings', (result) =>
+            resolve(result.extensionSettings || {})
+        )
+    );
+
+    console.log('extensionSettings', extensionSettings);
+
+    return extensionSettings;
+}
--- src/worker.js
+++ src/worker.js
@@ -1,60 +1,95 @@
-async function getUserSettings() {
-     return new Promise((resolve) =>
-          chrome.storage.local.get('userSettings', (result) => resolve(result.userSettings || {}))
-     );
-}
+import {
+    getExtensionSettings,
+    getUserSettings,
+    setExtensionSetting,
+    updateThemes,
+} from './shared.js';
 
 async function loadStylesInTab(tabId, url) {
-     const userSettings = await getUserSettings();
-     let count = 0;
+    const userSettings = await getUserSettings();
+    let count = 0;
 
-     if (Object.keys(userSettings).length === 0) return count;
+    if (Object.keys(userSettings).length === 0) return count;
 
-     for (const theme of Object.keys(userSettings)) {
-          const settings = userSettings[theme];
+    for (const theme of Object.keys(userSettings)) {
+        const settings = userSettings[theme];
 
-          if (!settings.checked || !settings.link) continue;
+        if (!settings.checked || !settings.link) continue;
 
-          const link = settings.link.replace('https://', '').toLowerCase();
+        const link = settings.link.replace('https://', '').toLowerCase();
 
-          if (url.includes(link) || url.includes('www.' + link) || link === '*') {
-               await chrome.scripting.insertCSS({
-                    target: {
-                         tabId: tabId,
-                         // allFrames: true,
-                    },
-                    css: settings.compiledCss,
-               });
-               count++;
-          }
-     }
+        if (url.includes(link) || url.includes('www.' + link) || link === '*') {
+            await chrome.scripting.insertCSS({
+                target: {
+                    tabId: tabId,
+                    // allFrames: true,
+                },
+                css: settings.compiledCss,
+            });
+            count++;
+        }
+    }
 
-     return count;
+    return count;
 }
 
 chrome.webNavigation.onCommitted.addListener(async (details) => {
-     if (
-          [
-               //
-               'reload',
-               'link',
-               'typed',
-               'generated',
-               'auto_bookmark',
-               'form_submit',
-          ].includes(details.transitionType)
-     ) {
-          const tabId = details.tabId;
-          const url = details.url;
+    if (
+        [
+            //
+            'reload',
+            'link',
+            'typed',
+            'generated',
+            'auto_bookmark',
+            'form_submit',
+        ].includes(details.transitionType)
+    ) {
+        const tabId = details.tabId;
+        const url = details.url;
 
-          if (url) {
-               try {
-                    const loaded = await loadStylesInTab(tabId, url);
-                    console.log(`Loaded ${loaded} styles:`, url, tabId);
-               } catch (e) {
-                    console.log('Failed to load styles in tab:', url, tabId);
-                    console.log('catch', e);
-               }
-          }
-     }
+        if (url) {
+            try {
+                const loaded = await loadStylesInTab(tabId, url);
+                console.log(`Loaded ${loaded} styles:`, url, tabId);
+            } catch (e) {
+                console.log('Failed to load styles in tab:', url, tabId);
+                console.log('catch', e);
+            }
+        }
+    }
 });
+
+async function checkForBackgroundUpdates() {
+    const extensionSettings = await getExtensionSettings();
+
+    const backgroundUpdate = async () => {
+        setExtensionSetting('lastThemesUpdate', new Date().getTime());
+
+        const userSettings = await getUserSettings();
+        updateThemes(userSettings);
+        console.log('themes updated in the background');
+    };
+
+    if (extensionSettings['backgroundUpdate']) {
+        const ALARM_NAME = 'UPDATE_THEMES';
+
+        async function createAlarm() {
+            const alarm = await chrome.alarms.get(ALARM_NAME);
+            if (typeof alarm === 'undefined') {
+                console.log('created alarm');
+                chrome.alarms.create(ALARM_NAME, {
+                    delayInMinutes: 1,
+                    periodInMinutes: 1440,
+                });
+                backgroundUpdate();
+            }
+        }
+
+        createAlarm();
+
+        chrome.alarms.onAlarm.addListener(backgroundUpdate);
+    }
+}
+
+checkForBackgroundUpdates();
